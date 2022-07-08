5b1c0cdfe82b135fc97c83d2196eff2845fe4f3d
Jakob Durstberger <jakob.durstberger@mypulse.ai>
2022-07-08 16:38:51 +0200

    Reformat files

--- spec/project_hierarchy_spec.rb
+++ spec/project_hierarchy_spec.rb
@@ -4,8 +4,9 @@ require 'spec_helper'
 
 describe 'project hierarchy' do
   context 'when provisioned with default' do
-
-    let(:deployment_identifier) { vars(:project_hierarchy).deployment_identifier }
+    let(:deployment_identifier) do
+      vars(:project_hierarchy).deployment_identifier
+    end
 
     it 'creates a root project' do
       found_project = resource_manager.projects.find do |i|
