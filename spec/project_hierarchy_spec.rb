4014ddd041b01d8a96b54a697585afa9973b13e4
Jordan Robinson <me@jordanrobinson.co.uk>
2022-07-13 15:43:48 +0100

    Linting fixes

--- spec/project_hierarchy_spec.rb
+++ spec/project_hierarchy_spec.rb
@@ -27,24 +27,46 @@ describe 'project hierarchy' do
     end
 
     it 'creates the management folder' do
+      management_folder_id = output(:project_hierarchy, 'management_folder_id')
+
+      client = Google::Cloud::ResourceManager::V3::Folders::Client.new do |conf|
+        conf.endpoint = 'cloudresourcemanager.googleapis.com'
+      end
+
+      expect do
+        request = Google::Cloud::ResourceManager::V3::GetFolderRequest.new
+        request.name = "folders/#{management_folder_id}"
+        client.get_folder(request)
+      end.not_to(raise_error)
+    end
+
+    it 'creates the management folder with the correct parent folder' do
       parent_folder_id = vars(:project_hierarchy).folder_id
       management_folder_id = output(:project_hierarchy, 'management_folder_id')
 
-      client = Google::Cloud::ResourceManager::V3::Folders::Client.new do |config|
-        config.endpoint = 'cloudresourcemanager.googleapis.com'
+      client = Google::Cloud::ResourceManager::V3::Folders::Client.new do |conf|
+        conf.endpoint = 'cloudresourcemanager.googleapis.com'
       end
 
-      folder = nil
+      request = Google::Cloud::ResourceManager::V3::GetFolderRequest.new
+      request.name = "folders/#{management_folder_id}"
+      folder = client.get_folder(request)
 
-      expect {
-        request = Google::Cloud::ResourceManager::V3::GetFolderRequest.new
-        request.name = "folders/#{management_folder_id}"
-        folder = client.get_folder(request)
-      }.not_to(raise_error)
+      expect(folder.parent).to eq("folders/#{parent_folder_id}")
+    end
 
-      expect(folder.parent).to eq('folders/' + parent_folder_id)
+    it 'creates the management folder with the correct name' do
+      management_folder_id = output(:project_hierarchy, 'management_folder_id')
+
+      client = Google::Cloud::ResourceManager::V3::Folders::Client.new do |conf|
+        conf.endpoint = 'cloudresourcemanager.googleapis.com'
+      end
+
+      request = Google::Cloud::ResourceManager::V3::GetFolderRequest.new
+      request.name = "folders/#{management_folder_id}"
+      folder = client.get_folder(request)
+
       expect(folder.display_name).to eq('management')
-
     end
 
     it 'creates a management project' do
@@ -55,10 +77,10 @@ describe 'project hierarchy' do
     end
 
     it 'outputs the management_project_id' do
-      management_project_id = output(:project_hierarchy, 'management_project_id')
+      management_project_id = output(:project_hierarchy,
+                                     'management_project_id')
 
       expect(management_project_id).to be_truthy
     end
-
   end
 end
